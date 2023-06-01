5b31ee8b285895d35a90af6d79c5c1cf9e8fa8f4
杨千里 <1696016522@qq.com>
2023-06-01 17:31:29 +0800

    feat：add

---
+++ src/views/landing/index.tsx
@@ -0,0 +1,13 @@
+import { ProLayout } from '@ant-design/pro-components';
+import { Button } from 'antd';
+
+export const Landing = () => {
+  return (
+    <>
+      <ProLayout title="react模板项目--无妻徒刑">
+        <Button type="primary">Button</Button>
+        <div>Landing</div>
+      </ProLayout>
+    </>
+  )
+}
